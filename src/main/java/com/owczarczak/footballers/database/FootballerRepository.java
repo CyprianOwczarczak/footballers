4b073aee60b7d0dadc209baea9f03b7976a912cd
Cyprian <cyprian.owczarczak@gmail.com>
2023-01-17 10:00:48 +0100

    TODO

--- src/main/java/com/owczarczak/footballers/database/FootballerRepository.java
+++ src/main/java/com/owczarczak/footballers/database/FootballerRepository.java
@@ -4,5 +4,7 @@ import com.owczarczak.footballers.model.Footballer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FootballerRepository extends JpaRepository<Footballer, Integer> {
+    //TODO No podstawie nazwy metody Spring generuje kod
+    //Drugi sposób -> native SQl query/ query JPA/ criteria builder (Otomoto)
 
 }
