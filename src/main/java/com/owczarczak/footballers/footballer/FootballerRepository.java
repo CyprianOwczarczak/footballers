dceec8e3375dd535be344b1c59ac3f8703d81e03
Cyprian <dranags@gmail.com>
2023-02-01 20:50:28 +0100

    Changed nativeQuery to JPQL

--- src/main/java/com/owczarczak/footballers/footballer/FootballerRepository.java
+++ src/main/java/com/owczarczak/footballers/footballer/FootballerRepository.java
@@ -7,21 +7,12 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface FootballerRepository extends JpaRepository<Footballer, Integer> {
-    @Query(value = """
-            select *
-            from footballers
-            order by height desc
-            limit 3
-            """, nativeQuery = true)
-    List<Footballer> get3HighestFootballers();
 
-    @Query(value = """
-            select *
-            from footballers
-            where 'name' like '%search%'
-            """, nativeQuery = true)
-        //FIXME JPQL zamiast nativeQuery !
-    List<Footballer> getFootballersByName(@Param("search") String name);
+    @Query("select * from footballers order by height desc limit 3")
+    List<FootballerDto> get3HighestFootballers();
 
-    List<Footballer> findByName(@Param("name") String name);
+    @Query("select * form footballers where 'name' like '%search%'")
+    List<FootballerDto> getFootballersByName(@Param("search") String name);
+
+    List<FootballerDto> findByName(@Param("name") String name);
 }
