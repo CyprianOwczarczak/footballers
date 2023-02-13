e1f667eb97bb81a07a71d738b0e2cbc9917efdbe
Cyprian <cyprian.owczarczak@gmail.com>
2023-02-13 12:06:26 +0100

    New Todo

--- src/test/java/com/owczarczak/footballers/footballer/FootballerControllerTest.java
+++ src/test/java/com/owczarczak/footballers/footballer/FootballerControllerTest.java
@@ -12,12 +12,6 @@ import static org.mockito.Mockito.when;
 
 class FootballerControllerTest {
 
-    private final FootballerRepository mockRepository = mock(FootballerRepository.class);
-    private final FootballerService mockService = mock(FootballerService.class);
-//	private final FootballerMapper mapper = new FootballerMapper();
-
-    private List<Footballer> footballerList = new ArrayList<>();
-
     @Test
     @DisplayName("Should add a footballer")
     void shouldAddFootballer() {
