37e96f84a3fd476e33d6cee35aa3596ffc8db454
Cyprian <cyprian.owczarczak@gmail.com>
2023-02-13 12:55:27 +0100

    TODO

--- src/test/java/com/owczarczak/footballers/footballer/FootballerControllerTest.java
+++ src/test/java/com/owczarczak/footballers/footballer/FootballerControllerTest.java
@@ -2,19 +2,39 @@ package com.owczarczak.footballers.footballer;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.util.ArrayList;
-import java.util.List;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration
+@WebAppConfiguration
+@AutoConfigureMockMvc
 class FootballerControllerTest {
 
+    @Autowired
+    MockMvc mockMvc;
+
     @Test
-    @DisplayName("Should add a footballer")
-    void shouldAddFootballer() {
+    @DisplayName("Should startup Spring")
+    void shouldStartupSpring() {
+    }
+
+    @Test
+    void shouldGetAllFootballers() throws Exception {
+        this.mockMvc.perform(get("/footballers/"))
+                .andDo(print())
+                .andExpect(status().is2xxSuccessful());
+
     }
 
 }
