a98e1113e5353954e05033f6413187b196f3d8af
Cyprian <dranags@gmail.com>
2023-02-15 00:14:00 +0100

    Adding more controller tests

--- src/test/java/com/owczarczak/footballers/footballer/FootballerControllerTest.java
+++ src/test/java/com/owczarczak/footballers/footballer/FootballerControllerTest.java
@@ -1,20 +1,19 @@
 package com.owczarczak.footballers.footballer;
 
-import org.aspectj.lang.annotation.Before;
-import org.junit.jupiter.api.*;
-import org.mockito.Mock;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.hasSize;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.hamcrest.Matchers.isEmptyString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -28,6 +27,19 @@ class FootballerControllerTest {
     // Should delete a footballer --> return 2xx, check if footballer with that id doesn't exist anymore
     // Should fail to delete a footballer --> footballer with that id doens't exist already,
     // check the size of the footballerList before and after deletion
+
+    //TODO
+//    mockMvc.perform(get("/api/deliverypoints/?customerIds=" + ids))
+//            .andDo(print())
+//            .andExpectAll(
+//            content().contentType(MediaType.APPLICATION_JSON),
+//    status().is2xxSuccessful(),
+//    jsonPath("$").isArray(),
+//    jsonPath("$", hasSize(3)),
+//    jsonPath("$[0].address.street", is("Św. Antoniego")),
+//    jsonPath("$[0].name", is("Fabryka w Poznaniu")),
+//    jsonPath("$[1].address.city", is("Wrocław")),
+//    jsonPath("$[1].name", is("Fabryka w mieście krasnali")));
 
     @Autowired
     FootballerRepository repository;
@@ -58,9 +70,11 @@ class FootballerControllerTest {
     @Test
     @DisplayName("Should get a footballer by id")
     void shouldGetFootballerById() throws Exception {
-        this.mockMvc.perform(get("/footballers/6"))
+        Footballer footballer1 = new Footballer("111111", "testPlayer", "testClub", 10, 150);
+        repository.save(footballer1);
+        this.mockMvc.perform(get("/footballers/1"))
                 .andDo(print())
-                .andExpect(status().is2xxSuccessful());
+                .andExpectAll(status().is2xxSuccessful());
     }
 
     // Should fail to get footballer by id -->return 4xx not found
@@ -76,8 +90,8 @@ class FootballerControllerTest {
     @Test
     @DisplayName("Should get footballers by name")
     void shouldGetFootballersByName() throws Exception {
-        Footballer footballer = new Footballer(1234, "123456", "testPlayer", "testClub", 20, 180);
-        repository.save(footballer);
+        Footballer footballer1 = new Footballer("111111", "testPlayer", "testClub", 10, 150);
+        repository.save(footballer1);
         //Get footballersList and check the size
         this.mockMvc.perform(get("/footballers/byName/?name=testPlayer"))
                 .andDo(print())
@@ -88,50 +102,42 @@ class FootballerControllerTest {
     @Test
     @DisplayName("Should get multiple footballers by name")
     void shouldGetMultipleFootballersByName() throws Exception {
-        Footballer footballer1 = new Footballer("123456", "testPlayer", "testClub", 20, 180);
-        Footballer footballer2 = new Footballer("234567", "testPlayer", "testClub", 20, 180);
-        Footballer footballer3 = new Footballer("345678", "testPlayer", "testClub", 20, 180);
+        Footballer footballer1 = new Footballer("111111", "testPlayer", "testClub", 10, 150);
+        Footballer footballer2 = new Footballer("222222", "testPlayer", "testClub2", 20, 160);
+        Footballer footballer3 = new Footballer("333333", "testPlayer2", "testClub3", 30, 170);
         repository.save(footballer1);
         repository.save(footballer2);
         repository.save(footballer3);
         this.mockMvc.perform(get("/footballers/byName/?name=testPlayer"))
                 .andDo(print())
-                .andExpect(status().is2xxSuccessful());
+                .andExpectAll(status().is2xxSuccessful(),
+                        jsonPath("$", hasSize(2)));
     }
 
-    //TODO
-//    mockMvc.perform(get("/api/deliverypoints/?customerIds=" + ids))
-//            .andDo(print())
-//            .andExpectAll(
-//            content().contentType(MediaType.APPLICATION_JSON),
-//    status().is2xxSuccessful(),
-//    jsonPath("$").isArray(),
-//    jsonPath("$", hasSize(3)),
-//    jsonPath("$[0].address.street", is("Św. Antoniego")),
-//    jsonPath("$[0].name", is("Fabryka w Poznaniu")),
-//    jsonPath("$[1].address.city", is("Wrocław")),
-//    jsonPath("$[1].name", is("Fabryka w mieście krasnali")));
 
+    //Should not get any footballers by name --> check if the list is empty
+    @Test
+    @DisplayName("Should not get any footballers by name")
+    void shouldNotGetAnyFootballersByName() throws Exception {
+        //Get footballersList and check is the size is 0
+        this.mockMvc.perform(get("/footballers/byName/?name=xyz"))
+                .andDo(print())
+                .andExpectAll(
+                        jsonPath("$").isArray(),
+                        jsonPath("$", hasSize(0))
+                );
+    }
 
-    // Should not get any footballers by name --> check if the list is empty
-//    @Test
-//    @DisplayName("Should not get any footballers by name")
-//    void shouldNotGetAnyFootballersByName() throws Exception {
-//        //Get footballersList and check is the size is 0
-//        this.mockMvc.perform(get("/footballers/byName/?name=xyz"))
-//                .andDo(print())
-//                .andExpect(status().is4xxClientError());
-//    }
-
-//     Should add a footballer --> return 2xx created, check if the player was added and if he has the parameters we wanted
+    //     Should add a footballer --> return 2xx created, check if the player was added and if he has the parameters we wanted
     @Test
     @DisplayName("Should add a footballer")
     void shouldAddFootballer() throws Exception {
-        Footballer footballer = new Footballer(53527, "4125", "testPlayer", "testClub", 20, 180);
+        Footballer footballer = new Footballer("111111", "testPlayer", "testClub", 20, 180);
         int returnedFootballerId = repository.save(footballer).getId();
         this.mockMvc.perform(get("/footballers/" + returnedFootballerId))
                 .andDo(print())
-                .andExpect(status().is2xxSuccessful());
+                .andExpectAll(status().isCreated(),
+                        jsonPath("$", hasSize(1)));
     }
 
     // Should fail to add a footballer -->  footballer not added, wrong body passed or the id already exists
@@ -142,13 +148,23 @@ class FootballerControllerTest {
         int returnedFootballerId = repository.save(footballer).getId();
         this.mockMvc.perform(get("/footballers/" + returnedFootballerId))
                 .andDo(print())
-                .andExpect(status().is4xxClientError());
+                .andExpect(jsonPath("$", isEmptyString()));
     }
 
     // Should Get X Highest Footballers --> check if the list size is X
     @Test
     @DisplayName("Should get X highest footballers")
     void shouldGetXHighestFootballers() throws Exception {
+        Footballer footballer1 = new Footballer("111111", "testPlayer", "testClub", 10, 150);
+        Footballer footballer2 = new Footballer("222222", "testPlayer", "testClub2", 20, 160);
+        Footballer footballer3 = new Footballer("333333", "testPlayer", "testClub3", 30, 170);
+        repository.save(footballer1);
+        repository.save(footballer2);
+        repository.save(footballer3);
+        this.mockMvc.perform(get("/footballers/topXByHeight/?pageNumber=0&numberOfPlayers=2"))
+                .andDo(print())
+                .andExpectAll(jsonPath("$", hasSize(2)));
+        //todo add check for the highest and second highest footballer
     }
 
     // Shouldn't get more than X footballers --> check if it doesn't return more footballers
