d63c94c477f097c253536b578316cc47ef952ce6
Cyprian <cyprian.owczarczak@gmail.com>
2023-02-16 13:04:39 +0100

    Fixed Flyway folder path

--- src/main/resources/db/migration/V1__initial_configuration.sql
+++ src/main/resources/db/migration/V1__initial_configuration.sql
@@ -1,12 +1,10 @@
-drop table if exists footballers;
-
 create table footballers (
 	id serial,
-	pesel varchar(255),
-	name varchar(255),
+	pesel varchar(255) not null,
+	name varchar(255) not null,
 	club varchar(255),
 	goals int,
-	height int,
+	height int not null,
 	saves int,
 	primary key (id),
 	unique (pesel)
